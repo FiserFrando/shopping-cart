a6f43d5ec03ce9b7e637c7d3bbbfabc915785110
fiser <fiser.frando@gmail.com>
2023-03-29 00:38:51 -0300

    database 2

--- database.py
+++ database.py
@@ -48,7 +48,7 @@ class Product(Model):
 
 class Category(Model):
     name = IntegerField()
-    created_at = IntegerField()
+    created_at = DateTimeField(default=datetime.now())
 
     class Meta:
         database = database
@@ -58,4 +58,22 @@ class Category(Model):
 class Shopping_cart_products(Model):
     Shopping_cart_id = ForeignKeyField(Shopping_cart, backref='shopping_cart_products')
     Product_id = ForeignKeyField(Product, backref='shopping_cart_products')
-    
+    quantity_products = IntegerField()
+    created_at = DateTimeField(default=datetime.now())
+
+    class Meta():
+        database = database
+        db_table = 'shopping_cart_products'
+
+
+class Product_category(Model):
+    product_id = ForeignKeyField(Product, backref='product_categories')
+    category_id = ForeignKeyField(Category, backref='product_categories')
+    created_at = DateTimeField(default=datetime.now())
+
+    class Meta:
+        database = database
+        db_table = 'product_categories'
+
+
+database.create_tables([User, Shopping_cart, Shopping_cart_products, Product, Product_category, Category])
