e6cc8b659e50665984df6c9ceae2246d4ff1c65c
fiser <fiser.frando@gmail.com>
2023-03-30 00:18:48 -0300

    more css and htmls

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template, request, session
+from datetime import datetime
+
+from flask import Flask, redirect, render_template, request, session
+
+from database import Category, Product, Shopping_cart, User
 
 app = Flask(__name__)
 app.secret_key = (
@@ -11,14 +15,31 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    return render_template("login.html")
-
-
 @app.route("/register", methods=["GET", "POST"])
 def register():
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        try:
+            user = User.get((User.email == email) and (User.password == password))
+            login = datetime.now()
+
+            user.last_login = login
+            user.save()
+
+            session["user_id"] = user.id
+            return redirect("/home")
+
+        except:
+            return render_template("no-login.html")
+
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
