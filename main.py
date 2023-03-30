69b5aacefa6171a7d503f1795c50bf2440409088
fiser <fiser.frando@gmail.com>
2023-03-29 22:44:18 -0300

    css added

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("hola mundo")
+from flask import Flask, render_template, request, session
+
+app = Flask(__name__)
+app.secret_key = (
+    'asflknjkJKIU/&/()h976789BH/(&%)()ghG(/&%//GR$&%T/gjhkjHiohbhjgGGYom!"#!")'
+)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
