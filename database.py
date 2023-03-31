1d4f63987a805213460db2f4c8a59734582ceb97
fiser <fiser.frando@gmail.com>
2023-03-31 00:26:47 -0300

    categories and rol admin

--- database.py
+++ database.py
@@ -1,39 +1,46 @@
 from datetime import datetime
 
 from decouple import config
-from peewee import (DateTimeField, ForeignKeyField, IntegerField, Model,
-                    MySQLDatabase, TextField)
+from peewee import (
+    DateTimeField,
+    ForeignKeyField,
+    IntegerField,
+    Model,
+    MySQLDatabase,
+    TextField,
+)
 
 database = MySQLDatabase(
-    'shopping-cart',
-    user = 'root',
-    password = config('dbpassword'),
-    port = 3306,
-    host = 'localhost'
+    "shopping_cart",
+    user="root",
+    password=config("dbpassword"),
+    port=3306,
+    host="localhost",
 )
 
 
 class User(Model):
-    name = TextField()
+    username = TextField()
     email = TextField()
     password = TextField()
+    rol = TextField()
     created_at = DateTimeField(default=datetime.now())
-    last_login = DateTimeField
+    last_login = DateTimeField()
 
     class Meta:
         database = database
-        db_table = 'users'
+        db_table = "users"
 
 
 class Shopping_cart(Model):
-    user_id = ForeignKeyField(User, backref='shopping_carts')
+    user_id = ForeignKeyField(User, backref="shopping_carts")
     total_price = IntegerField()
     created_at = DateTimeField(default=datetime.now())
     updated_at = DateTimeField()
 
     class Meta:
         database = database
-        db_table = 'shopping_carts'
+        db_table = "shopping_carts"
 
 
 class Product(Model):
@@ -43,37 +50,39 @@ class Product(Model):
 
     class Meta:
         database = database
-        db_table = 'products'
+        db_table = "products"
 
 
 class Category(Model):
-    name = IntegerField()
+    name = TextField()
     created_at = DateTimeField(default=datetime.now())
 
     class Meta:
         database = database
-        db_table = 'categories'
+        db_table = "categories"
 
 
 class Shopping_cart_products(Model):
-    Shopping_cart_id = ForeignKeyField(Shopping_cart, backref='shopping_cart_products')
-    Product_id = ForeignKeyField(Product, backref='shopping_cart_products')
+    Shopping_cart_id = ForeignKeyField(Shopping_cart, backref="shopping_cart_products")
+    Product_id = ForeignKeyField(Product, backref="shopping_cart_products")
     quantity_products = IntegerField()
-    created_at = DateTimeField(default=datetime.now())
-
-    class Meta():
-        database = database
-        db_table = 'shopping_cart_products'
-
-
-class Product_category(Model):
-    product_id = ForeignKeyField(Product, backref='product_categories')
-    category_id = ForeignKeyField(Category, backref='product_categories')
     created_at = DateTimeField(default=datetime.now())
 
     class Meta:
         database = database
-        db_table = 'product_categories'
+        db_table = "shopping_cart_products"
 
 
-database.create_tables([User, Shopping_cart, Shopping_cart_products, Product, Product_category, Category])
+class Product_category(Model):
+    product_id = ForeignKeyField(Product, backref="product_categories")
+    category_id = ForeignKeyField(Category, backref="product_categories")
+    created_at = DateTimeField(default=datetime.now())
+
+    class Meta:
+        database = database
+        db_table = "product_categories"
+
+
+database.create_tables(
+    [User, Shopping_cart, Shopping_cart_products, Product, Product_category, Category]
+)
--- main.py
+++ main.py
@@ -17,6 +17,27 @@ def index():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        login = datetime.now()
+
+        if User.select().where(User.email == email):
+            return render_template("email-exists.html", email=email)
+
+        if username and email and password:
+            user = User.create(
+                username=username,
+                email=email,
+                password=password,
+                last_login=login,
+                rol="user",
+            )
+            session["user_id"] = user.id
+            session["user_rol"] = "user"
+            return redirect("/home")
+
     return render_template("register.html")
 
 
@@ -34,12 +55,124 @@ def login():
             user.save()
 
             session["user_id"] = user.id
+
+            if user.rol == "admin":
+                session["user_rol"] = "admin"
+            else:
+                session["user_rol"] = "user"
+
             return redirect("/home")
 
         except:
             return render_template("no-login.html")
 
     return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    return render_template("/logout.html")
+
+
+@app.route("/logoutme")
+def logoutme():
+    if not session.get("user_id"):
+        return redirect("/")
+
+    session.clear()
+    return redirect("/")
+
+
+@app.route("/home")
+def home():
+    if not session.get("user_id"):
+        return redirect("/")
+
+    if session["user_rol"] == "user":
+        return render_template("home.html")
+    else:
+        return render_template("/admin/home.html")
+
+
+@app.route("/categories")
+def categories():
+    if not session.get("user_id"):
+        return redirect("/")
+
+    categories = Category.select()
+
+    return render_template("/categories/index.html", categories=categories)
+
+
+####################################################################################################
+
+
+@app.route("/categories/create", methods=["GET", "POST"])
+def pcategory_create():
+    if not session.get("user_id"):
+        return redirect("/")
+
+    if request.method == "POST":
+        name = request.form.get("name")
+        print(name)
+
+        if name:
+            Category.create(name=name)
+            return redirect("/categories")
+
+    return render_template("categories/create.html")
+
+
+@app.route("/categories/update/<id>", methods=["GET", "POST"])
+def product_update(id):
+    if not session.get("user_id"):
+        return redirect("/")
+
+    category = Category.get(Category.id == id)
+
+    if request.method == "POST":
+        name = request.form.get("name")
+
+        if name:
+            category.name = name
+
+            category.save()
+
+            return redirect("/categories")
+
+    return render_template("/categories/update.html", category=category)
+
+
+@app.route("/categories/delete/<id>", methods=["GET", "POST"])
+def category_delete(id):
+    if not session.get("user_id"):
+        return redirect("/")
+
+    category = Category.get(Category.id == id)
+
+    if request.method == "POST":
+        si = request.form.get("si")
+        no = request.form.get("no")
+
+        if no:
+            return redirect("/categories")
+
+        if si:
+            category.delete_instance()
+            return redirect("/categories")
+
+    return render_template("categories/delete.html", category=category)
+
+
+##############################################################################################################
+
+
+@app.route("/products")
+def products():
+    if not session.get("user_id"):
+        return redirect("/")
+
+    return render_template("products.html")
 
 
 if __name__ == "__main__":
